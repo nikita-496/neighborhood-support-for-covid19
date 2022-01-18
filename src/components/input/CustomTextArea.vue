2d5aad1d1b4b15043eed892ed18cd4e591b6f8a6
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-01-18 19:57:15 +0500

    creation custom ui element components

---
+++ src/components/input/CustomTextArea.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-textarea v-model="content" solo name="input-7-4" label="Solo textarea" autofocus>
+    </v-textarea>
+    <v-btn @click="save">Создать</v-btn>
+    <v-btn @click="cancel">Отмена</v-btn>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => {
+      return {
+        content: "",
+      };
+    },
+    methods: {
+      save() {
+        this.$emit("saveText", this.content);
+        this.$emit("closeTextArea", false);
+      },
+      cancel() {
+        this.$emit("cancelChanges");
+      },
+    },
+  };
+</script>
